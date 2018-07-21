85b0f8601631258c00d71787fa727a01edf78131
Sebastian Vera Garcia <38116582+Etzyy@users.noreply.github.com>
2018-07-21 17:30:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const ytdl = require('ytdl-core');
+const search = require('yt-search');
+const fs = require('fs');
+const prompt = require('prompt-base');
+const colors = require('colors');
+
+process.title = "Youtube Downloader by Etzyy"
+
+let term = new prompt({
+    name: 'term',
+    message: 'Please, input an url or search term:'
+})
+
+let format = new prompt({
+    name: 'format',
+    message: 'Please, input a format (mp3/mp4):'
+})
+
+let choose = new prompt({
+    name: 'choose',
+    message: "What video would you like?"
+})
+
+function askFormat(ter) {
+    format.run()
+        .then(async form => {
+            let formath;
+            if (typeof form !== "string") return askFormat();
+            if (form.startsWith("mp4")) formath = "video"
+            else if (form.startsWith("mp3")) formath = "audioonly"
+            else askFormat(ter)
+            download(ter, formath, form)
+        })
+}
+
+function askTerm() {
+    term.run()
+        .then(async ter => {
+            askFormat(ter)
+        })
+}
+
+askTerm()
+
+async function download(ter, form, extension) {
+    if (typeof ter !== "string") {
+        return askFormat(ter); // if term is not a string then return to start
+    }
+    if (!ytdl.validateURL(ter)) {
+        console.clear()
+        search(ter, (err, res) => {
+            if (err) return console.log('Could not get that search term! Please be more specific.'.bgRed)
+            let videos = res.videos.slice(0, 10)
+            if (videos.length < 1) return console.log('Could not get the search term. Please be more specify.'.bgRed)
+            let resp = '';
+            for (var i in videos) {
+                resp += `[${parseInt(i)+1}]: "${videos[i].title}"\n`
+            }
+            resp += `\nChoose a number between 1-${videos.length}. To cancel use "0"`;
+            console.log(resp)
+            choose.run().then(async num => {
+                let ch = parseInt(num);
+                if (ch == 0) return askTerm();
+                // [videos[ch - 1].url]
+                console.clear()
+                let info = await ytdl.getInfo(videos[parseInt(num) - 1].url)
+                // console.log(info)
+                console.log(`"${info.title}" by "${info.author.name}" is being downloaded..`.bold)
+
+                await ytdl(videos[parseInt(num) - 1].url, {
+                        filter: form
+                    })
+                    .pipe(fs.createWriteStream(`Downloaded/${info.title}.${extension}`)).on('close', () => {
+                        console.log('\nThanks for downloading with us!'.rainbow)
+
+                    })
+            })
+        })
+    } else {
+        console.clear()
+        let info = await ytdl.getInfo(ter)
+        // console.log(info)
+        console.log(`"${info.title}" by "${info.author.name}" is being downloaded..`.bold)
+
+        await ytdl(ter, {
+                filter: form
+            })
+            .pipe(fs.createWriteStream(`./Downloaded/${info.title}.${extension}`)).on('close', () => {
+                console.log('Thanks for downloading with us!'.rainbow)
+
+            })
+    }
+}
